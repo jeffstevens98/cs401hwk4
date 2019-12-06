c345fdfcba0e242274befd882820a150c6bbe141
jeff stevens <steven_j1@denison.edu>
2019-12-05 23:52:56 -0500

    bouta submit

--- abc.cpp
+++ abc.cpp
@@ -17,7 +17,7 @@
 #include"functions.cpp"
 
 
-void printVec(std::vector< std::vector<double> > vec)
+void printVecOfVecs(std::vector< std::vector<double> > vec)
 //Prints the contents of a vector. Used for debugging.
 {
 	printf("vector size after being passed in %d\n", vec.size());
@@ -29,6 +29,15 @@ void printVec(std::vector< std::vector<double> > vec)
 			printf(" %f ",vec[i][j]);
 		}
 		printf("\n");
+	}
+}
+
+
+void printVec(std::vector<double> vec)
+{
+	for(int i = 0; i < vec.size(); i++)
+	{
+		std::cout << vec[i] << "\n";
 	}
 }
 
@@ -63,7 +72,7 @@ int randomBeeWithException(int exception, int range)
 }
 
 
-std::vector<double> rouletteSelection(std::vector< std::vector<double> > solutionPopulation, std::vector<double> fitnesses, std::string minOrMax = "max")
+std::vector<double> rouletteSelection(std::vector< std::vector<double> > &solutionPopulation, std::vector<double> fitnesses)//, std::string minOrMax)
 /* Returns a solution from the population of solutions based upon a selection where the  probability of selection is dependent upon the fitness of a solution.
  * solutionPopulation - the solutions that the selection will be performed upon
  * fitnesses - a vector of each fitness value of each solution
@@ -89,14 +98,7 @@ std::vector<double> rouletteSelection(std::vector< std::vector<double> > solutio
 	DEBUG? printf("initialized vectors for probability ranges\n") : 0;
 	for(int i = 0; i < probabilities.size(); i++)
 	{
-		if (minOrMax == "max")
-		{
-			probabilities[i] = (fitnesses[i])/(sumOfAllFitnesses);
-		}
-		if (minOrMax == "min")
-		{
-			probabilities[i] = (1/fitnesses[i])/(sumOfAllFitnesses);
-		}
+		probabilities[i] = (1/fitnesses[i])/(sumOfAllFitnesses);
 		probabilityRanges.push_back(nestedVec);
 		probabilityRanges.back()[0] = upperBoundOfProbability;
 		probabilityRanges.back()[1] = upperBoundOfProbability + probabilities[i];
@@ -104,11 +106,12 @@ std::vector<double> rouletteSelection(std::vector< std::vector<double> > solutio
 	}
 	DEBUG? printf("probability ranges calculated\n") : 0;
 	//Select a random number between zero and one
-	double random = static_cast <double> (rand() /( static_cast <double> (RAND_MAX/(1))));
+	double random = static_cast <double> (rand() /( static_cast <double> (RAND_MAX/(probabilityRanges[probabilityRanges.size()-1][1]))));
 	int chosenIndex = -1; //the index that will be chosen
 	//Iterate through our ranges of probabilities to pick the correct one
 	for(int i = 0; i < probabilityRanges.size(); i++)
 	{
+		//DEBUG? std::cout << random << "  lower bound: " << probabilityRanges[i][0] << "  upper bound: " << probabilityRanges[i][1] << "\n" : 0;
 		if ((random >= probabilityRanges[i][0]) && (random < probabilityRanges[i][1]))
 		{
 			DEBUG? printf("solution picked!\n") : 0;
@@ -121,7 +124,7 @@ std::vector<double> rouletteSelection(std::vector< std::vector<double> > solutio
 }
 
 
-std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchSpaceLowerBound, double searchSpaceUpperBound, int dimensions, std::string minOrMax = "max")
+std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchSpaceLowerBound, double searchSpaceUpperBound, int dimensions, std::string minOrMax)
 /* The artificial bee colony algorithm - Used to optimize functions.
  * (*fitnessFunction)() - a function pointer that is used to pass the names of functions as parameters. These functions are the functions being optimized.
  * searchSpaceLowerBound - the lower bound of the search space for the function to be optimized
@@ -132,7 +135,6 @@ std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchS
 {
 	bool DEBUG = false;
 	DEBUG? printf("\nstarting abc algorithm...\n") : 0;
-	std::vector<double> bestSolution;
 	int numberOfEmployedBees = 100;
 	int numberOfOnlookerBees = numberOfEmployedBees;
 	std::vector< std::vector<double> > employedBees;
@@ -167,6 +169,11 @@ std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchS
 	int foodSourceLifespanLimit = 10; //The maximum amount of iterations a food source is allowed to be a part of the population without improving.
 	std::vector<double> fitnesses; //vector of all of the fitnesses of the employed bee population
 	fitnesses.resize(numberOfEmployedBees);
+	std::vector<double> bestSolution = employedBees[0]; //give best solution an initial value
+	double bestSolutionArray2[2];
+	double bestSolutionArray10[10];
+	double fitnessCheckArray2[2]; //used to input the values from our vectors into the fitness functions
+	double fitnessCheckArray10[10]; // ||
 	DEBUG? printf("starting main algorithm...\n") : 0;
 	while(iter < 100) //tentative aspiration critera
 	{
@@ -178,6 +185,14 @@ std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchS
 			double betweenNeg1and1 = (-1.0) + static_cast <double> (rand() /( static_cast <double> (RAND_MAX/(1.0-(-1.0)))));//random float between negative 1 and 1
 			int randomBee = randomBeeWithException(eB,numberOfEmployedBees); //Pick a random employed bee that's not eB, the number of the current employed bee.
 			employedBees[eB][randomDimension] = employedBees[eB][randomDimension] + (betweenNeg1and1 * (employedBees[eB][randomDimension] - employedBees[randomBee][randomDimension]));
+			if (employedBees[eB][randomDimension] > searchSpaceUpperBound)
+			{
+				employedBees[eB][randomDimension] = searchSpaceUpperBound;
+			}
+			else if (employedBees[eB][randomDimension] < searchSpaceLowerBound)
+			{
+				employedBees[eB][randomDimension] = searchSpaceLowerBound;
+			}
 		}
 		DEBUG? printf("local search of employed bees complete\n") : 0;
 		//Now, the onlooker bees learn about each food source from each of the employed bees. The onlooker bees then decide which food source they want to visit based on roulette selection.
@@ -224,161 +239,158 @@ std::vector<double> abcAlgorithm(std::string fitnessFunctionName, double searchS
 			}
 		}
 		DEBUG? printf("Fitness of new solutions evaluated\n") : 0;
+		//printVecOfVecs(onlookerBees);
 		for(int oB = 0; oB < numberOfOnlookerBees; oB++)
 		{
 			onlookerBees[oB] = rouletteSelection(employedBees,fitnesses);
+			
 		}
-		//If a solution has existed within our population for longer than the allowed food source lifespan, it is discarded and a scout bee will find a new one.
-		////////////////////
-		//////////////////// work on this right here, champ!
-		////////////////////
-		//Send the onlookers bees out as employed bees for the next iteration of the algorithm.
 		employedBees = onlookerBees;
+		
+		//FIND THE GLOBAL BEST PEE
+		DEBUG? printf("Evaluating our population to find our best solution now\n") : 0;
+		for(int eB = 0; eB < employedBees.size(); eB++)
+		{
+			if (minOrMax == "max")
+			{
+				if (fitnessFunctionName == "Ackley")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Ackley(fitnessCheckArray2) > Ackley(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Ackley(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Easom")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Easom(fitnessCheckArray2) > Easom(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Easom(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Rastrigin")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Rastrigin(fitnessCheckArray2) > Rastrigin(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Rastrigin(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Sphere")
+				{
+					double beeArray[10];
+					double bestSolutionArray[10];
+					for(int i = 0; i < 10; i++)
+					{
+						beeArray[i] = employedBees[eB][i];
+						bestSolutionArray[i] = bestSolution[i];
+					}
+					if (Sphere(beeArray) > Sphere(bestSolutionArray))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Sphere(bestSolutionArray10));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "SumOfPowers")
+				{
+					for(int i = 0; i < 10; i++)
+					{
+						fitnessCheckArray10[i] = employedBees[eB][i];
+						bestSolutionArray10[i] = bestSolution[i];
+					}
+					if (SumOfPowers(fitnessCheckArray10) > SumOfPowers(bestSolutionArray10))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, SumOfPowers(bestSolutionArray10));
+				 		bestSolution = employedBees[eB];
+					}
+				}
+			}
+			else if (minOrMax == "min")
+			{
+				if (fitnessFunctionName == "Ackley")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Ackley(fitnessCheckArray2) < Ackley(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Ackley(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Easom")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Easom(fitnessCheckArray2) < Easom(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Easom(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Rastrigin")
+				{
+					fitnessCheckArray2[0] = employedBees[eB][0];
+					fitnessCheckArray2[1] = employedBees[eB][1];
+					bestSolutionArray2[0] = bestSolution[0];
+					bestSolutionArray2[1] = bestSolution[1];
+					if (Rastrigin(fitnessCheckArray2) < Rastrigin(bestSolutionArray2))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Rastrigin(bestSolutionArray2));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "Sphere")
+				{
+					double beeArray[10];
+					double bestSolutionArray[10];
+					for(int i = 0; i < 10; i++)
+					{
+						beeArray[i] = employedBees[eB][i];
+						bestSolutionArray[i] = bestSolution[i];
+					}
+					if (Sphere(beeArray) < Sphere(bestSolutionArray))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, Sphere(bestSolutionArray10));
+						bestSolution = employedBees[eB];
+					}
+				}
+				else if (fitnessFunctionName == "SumOfPowers")
+				{
+					for(int i = 0; i < 10; i++)
+					{
+						fitnessCheckArray10[i] = employedBees[eB][i];
+						bestSolutionArray10[i] = bestSolution[i];
+					}
+					if (SumOfPowers(fitnessCheckArray10) < SumOfPowers(bestSolutionArray10))
+					{
+						printf("bestSolution changed at iteration %d, bestsolution = %f\n\n", iter, SumOfPowers(bestSolutionArray10));
+				 		bestSolution = employedBees[eB];
+					}
+				}
+			}
+		}
 		DEBUG? printf("Ending iteration and beginning next iteration\n\n") : 0;
 		iter++;
 	}
+	
 	//When the main iterations of the algorithm are complete, we use a greedy selection to return our best solution from the population.
-	DEBUG? printf("Evaluating our population to find our best solution now\n") : 0;
-	bestSolution = employedBees[0]; //give best solution an initial value
-	double bestSolutionArray2[2];
-	double bestSolutionArray10[10];
-	double fitnessCheckArray2[2]; //used to input the values from our vectors into the fitness functions
-	double fitnessCheckArray10[10]; // ||
-	for(int eB = 0; eB < employedBees.size(); eB++)
-	{
-		if (minOrMax == "max")
-		{
-			if (fitnessFunctionName == "Ackley")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Ackley(fitnessCheckArray2) >= Ackley(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Easom")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Easom(fitnessCheckArray2) >= Easom(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Rastrigin")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Rastrigin(fitnessCheckArray2) >= Rastrigin(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Sphere")
-			{
-				double beeArray[10];
-				double bestSolutionArray[10];
-				for(int i = 0; i < 10; i++)
-				{
-					beeArray[i] = employedBees[eB][i];
-					bestSolutionArray[i] = bestSolution[i];
-				}
-				if (Sphere(beeArray) >= Sphere(bestSolutionArray))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "SumOfPowers")
-			{
-				for(int i = 0; i < 10; i++)
-				{
-					fitnessCheckArray10[i] = employedBees[eB][i];
-					bestSolutionArray10[i] = bestSolution[i];
-				}
-				if (SumOfPowers(fitnessCheckArray10) >= SumOfPowers(bestSolutionArray10))
-				{
-			 		bestSolution = employedBees[eB];
-				}
-			}
-		}
-		else if (minOrMax == "min")
-		{
-			if (fitnessFunctionName == "Ackley")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Ackley(fitnessCheckArray2) <= Ackley(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Easom")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Easom(fitnessCheckArray2) <= Easom(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Rastrigin")
-			{
-				fitnessCheckArray2[0] = employedBees[eB][0];
-				fitnessCheckArray2[1] = employedBees[eB][1];
-				bestSolutionArray2[0] = bestSolution[0];
-				bestSolutionArray2[1] = bestSolution[1];
-				if (Rastrigin(fitnessCheckArray2) <= Rastrigin(bestSolutionArray2))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "Sphere")
-			{
-				double beeArray[10];
-				double bestSolutionArray[10];
-				for(int i = 0; i < 10; i++)
-				{
-					beeArray[i] = employedBees[eB][i];
-					bestSolutionArray[i] = bestSolution[i];
-				}
-				if (Sphere(beeArray) <= Sphere(bestSolutionArray))
-				{
-					DEBUG? printf("bestSolution changed\n") : 0;
-					bestSolution = employedBees[eB];
-				}
-			}
-			else if (fitnessFunctionName == "SumOfPowers")
-			{
-				for(int i = 0; i < 10; i++)
-				{
-					fitnessCheckArray10[i] = employedBees[eB][i];
-					bestSolutionArray10[i] = bestSolution[i];
-				}
-				if (SumOfPowers(fitnessCheckArray10) <= SumOfPowers(bestSolutionArray10))
-				{
-			 		bestSolution = employedBees[eB];
-				}
-			}
-		}
-	}
 	return bestSolution;
 }
 
@@ -389,13 +401,14 @@ int main()
 	srand(time(NULL)); //set random seed
 	
 	printf("Artificial Bee Colony Function Optimization Results (best minimizations)\n\n");
-	printf("Ackley Function: ");
+	printf("Ackley Function: \n");
 	std::vector<double> ackleyOptimized = abcAlgorithm("Ackley", -32.0, 32.0, 2, "min");
 	double ackleyArray[2];
 	ackleyArray[0] = ackleyOptimized[0];
 	ackleyArray[1] = ackleyOptimized[1];
 	printf("%f\nInput variables: %f, %f\n\n", Ackley(ackleyArray), ackleyOptimized[0], ackleyOptimized[1]);
-	printf("Sphere Function: ");
+	
+	printf("Sphere Function: \n");
 	std::vector<double> sphereOptimized = abcAlgorithm("Sphere", -25.0, 25.0, 10, "min");
 	double sphereArray[10];
 	for (int i = 0; i < 10; i++)
@@ -403,19 +416,22 @@ int main()
 		sphereArray[i] = sphereOptimized[i];
 	}
 	printf("%f\nInput variables: %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n\n", Sphere(sphereArray), sphereArray[0], sphereArray[1], sphereArray[2], sphereArray[3], sphereArray[4], sphereArray[5], sphereArray[6], sphereArray[7], sphereArray[8], sphereArray[9]);
-	printf("Easom Function: ");
-	std::vector<double> easomOptimized = abcAlgorithm("Easom", -5.12, 5.12, 2, "min");
+
+	printf("Easom Function: \n");
+	std::vector<double> easomOptimized = abcAlgorithm("Easom", -100.0, 100.0, 2, "min");
 	double easomArray[2];
 	easomArray[0] = easomOptimized[0];
 	easomArray[1] = easomOptimized[1];
 	printf("%f\nInput variables: %f, %f\n\n", Easom(easomArray), easomOptimized[0], easomOptimized[1]);
-	printf("Rastrigin Function: ");
+	
+	printf("Rastrigin Function: \n");
 	std::vector<double> rastriginOptimized = abcAlgorithm("Rastrigin", -100.0, 100.0, 2, "min");
 	double rastriginArray[2];
 	rastriginArray[0] = rastriginOptimized[0];
 	rastriginArray[1] = rastriginOptimized[1];
 	printf("%f\nInput variables: %f, %f\n\n", Rastrigin(rastriginArray), rastriginOptimized[0], rastriginOptimized[1]);
-	printf("Sum of Powers Function:");
+
+	printf("Sum of Powers Function: \n");
 	std::vector<double> sopOptimized = abcAlgorithm("SumOfPowers", -1, 1, 10, "min");
 	double sopArray[10];
 	for (int i = 0; i < 10; i++)
@@ -423,4 +439,5 @@ int main()
 		sopArray[i] = sopOptimized[i];
 	}
 	printf("%f\nInput Variables: %f, %f, %f, %f, %f, %f, %f, %f, %f, %f\n\n", SumOfPowers(sopArray), sopArray[0], sopArray[1], sopArray[2], sopArray[3], sopArray[4], sopArray[5], sopArray[6], sopArray[7], sopArray[8], sopArray[9]);
+
 }
