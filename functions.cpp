9359bcab3510b37c56e7f770a58a54830d2bc6fc
Dylan Quinn Tallman <tallma_d1@red1.mathsci.denison.edu>
2019-12-05 19:06:17 -0500

    Added second forms of all needed function calls to be scaleable. The second forms are the regular call with 2 at the end.

--- functions.cpp
+++ functions.cpp
@@ -13,8 +13,22 @@ double Ackley (double x[2]){
   double ret = -20;
   double temp;
   double temp2;
-  temp = exp(-0.2*sqrt(0.5*(pow(x[0],2)*pow(x[1],2))));
+  temp = exp(-0.2*sqrt(0.5*(pow(x[0],2)+pow(x[1],2))));
   temp2 = exp(0.5*(cos(2*PI*x[0]) + cos(2*PI*x[1])));
+  ret = ret * temp;
+  ret = ret - temp2;
+  ret = ret + E + 20;
+  return ret;
+}
+
+double Ackley2 (double * x, int cnt){
+  double ret = -20;
+  double temp = 0;
+  double temp2 = 0;
+  for (int i = 0; i < 10; i++){
+    temp = temp + 0.5*(pow(x[i],2));
+    temp2 = temp2 + (1/cnt)*cos(2*PI*x[i]);
+  }
   ret = ret * temp;
   ret = ret - temp2;
   ret = ret + E + 20;
@@ -38,7 +52,15 @@ double Rastrigin (double x[2]){ //if you want to scale up, add a dimension parm 
   return ret;
 }
 
-//10 Dimensions, decision space = [-25,25]
+double Rastrigin2 (double * x, int cnt){ //if you want to scale up, add a dimension parm and set dim equal to it
+  double ret = 10*cnt;
+  for (int i = 0; i < cnt; i++){
+    ret = ret + pow(x[0], cnt) - 10 * cos(2*PI*x[i]);
+  }
+  return ret;
+}
+
+//10 Dimensions, decision space = [-5,5]
 double Sphere (double x[10]){
   double ret = 0;
   for (int i = 0; i < 10; i++){
@@ -47,11 +69,29 @@ double Sphere (double x[10]){
   return ret;
 }
 
+double Sphere2 (double * x, int cnt){
+  double ret = 0;
+  for (int i = 0; i < cnt; i++){
+    ret = ret + pow(x[i], 2);
+  }
+  return ret;
+}
+
+
+
 //10 Dimensions, decision space = [1,-1]
 double SumOfPowers (double x[10]){
   double ret = 0;
   for (int i = 1; i <= 10; i++){
-    ret = ret + pow(x[i], i);
+    ret = ret + pow(abs(x[i]), i);
+  }
+  return ret;
+}
+
+double SumOfPowers2 (double * x, int cnt){
+  double ret = 0;
+  for (int i = 1; i <= cnt+1; i++){
+    ret = ret + pow(abs(x[i]), i);
   }
   return ret;
 }
@@ -59,13 +99,18 @@ double SumOfPowers (double x[10]){
 /*
 int main(){
   double x1[2] = {0,0};
-  printf("Ackley: %f\n", Ackley(x1));
   double x2[2] = {PI,PI};
+  double x3[10] = {0,0,0,0,0,0,0,0,0,0};
+  printf("Ackley: %f\n", Ackley(x1));
+  printf("Ackley2: %f\n", Ackley2(x3, 10));
   printf("Easom: %f\n", Easom(x2));
   printf("Rast: %f\n", Rastrigin(x1));
-  double x3[10] = {0,0,0,0,0,0,0,0,0,0};
+  printf("Rast2: %f\n", Rastrigin2(x3, 10));
   printf("Sphere: %f\n", Sphere(x3));
+  printf("Sphere2: %f\n", Sphere2(x1,2));
   printf("SOP: %f\n", SumOfPowers(x3));
+  double x4[2] = {0,0};
+  printf("SOP2: %f\n", SumOfPowers2(x4,2));
   return 0;
 }
 */
